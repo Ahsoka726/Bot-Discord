c9f4e10496a96272055c87a5d9356993a4a7e828
NawelleBedr <bedrouni.nawelle@gmail.com>
2023-02-16 09:36:41 +0100

    thrith commit

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    //////////////////////////
+    //token a récupérer sur l'onglet Bot de l'application discord
+    token: "c24fab4f5e1d622787576a03b60572de3f52d8b8852a1787bf3a040290cb6c49",
+    //Statut que l'on veux afficher sur le bot
+    botStatus: "Wesh", 
+    //id du serveur : sur votre serveur faire un clique droit sur le nom d'un chanel puis copier l'id
+    idserveur : "1037383636950990942",
+    //Client id à récupérer dans information sur l'application discord
+    clientId: "1064469997306511360",
+    //id de l'application a récupérer sur l'application discord
+    guildId: "1064469997306511360",
+
+        //////////////////////////
+}
+
+//Cf application discord->https://discord.com/developers/applications
